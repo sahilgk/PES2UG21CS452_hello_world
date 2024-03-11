a9b1eeea1bce9d7485515fbdee1d8e3c7b55dc37
SuI <113618331+sahilgk@users.noreply.github.com>
2024-03-11 20:49:51 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!\n" << std::endl;
+    std::cout << "This is PES2UG21CS452\n" << std::endl;
     return 0;
 }
