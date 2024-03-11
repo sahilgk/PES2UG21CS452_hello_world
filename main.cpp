73f1ed4aec85e4dcc532855b6fdeaea6f7601934
SuI <113618331+sahilgk@users.noreply.github.com>
2024-03-11 20:59:48 +0530

    Update main.cpp
    updated the .cpp file to check build

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello, world!\n" << std::endl;
-    std::cout << "This is PES2UG21CS452\n" << std::endl;
+    std::cout << "Hello, world!\n This is PES2UG21CS452" << std::endl;
     return 0;
 }
